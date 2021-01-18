0f8c8bd9e5fd0e1490ad7a36dc4dd91ec90bf820
Robert Coleman <git@robert.net.nz>
2021-01-19 12:23:48 +1300

    update whitespace

--- app/code/community/Faonni/Compare/Helper/Product/Compare.php
+++ app/code/community/Faonni/Compare/Helper/Product/Compare.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Faonni
- *  
+ *
  * NOTICE OF LICENSE
  *
  * This source file is subject to the Open Software License (OSL 3.0)
@@ -14,13 +14,12 @@
  *
  * Do not edit or add to this file if you wish to upgrade module to newer
  * versions in the future.
- * 
+ *
  * @package     Faonni_Compare
- * @copyright   Copyright (c) 2015 Karliuka Vitalii(karliuka.vitalii@gmail.com) 
+ * @copyright   Copyright (c) 2015 Karliuka Vitalii(karliuka.vitalii@gmail.com)
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class Faonni_Compare_Helper_Product_Compare
-	extends Mage_Catalog_Helper_Product_Compare
+class Faonni_Compare_Helper_Product_Compare extends Mage_Catalog_Helper_Product_Compare
 {
     /**
      * Retrieve url for adding product to conpare list
@@ -30,9 +29,9 @@ class Faonni_Compare_Helper_Product_Compare
      */
     public function getAddUrl($product)
     {
-		if (Mage::helper('faonni_compare')->isEnabled()) {
+        if (Mage::helper('faonni_compare')->isEnabled()) {
             return parent::getAddUrl($product);
         }
-        return false;        
+        return false;
     }
 }
